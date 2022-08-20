a8b8af8b3aaa7d88d7973c7f8c0e4bb5f604a1ff
Joe Wandy <joewandy@gmail.com>
2022-08-20 15:31:26 +0100

    Script update

--- tune_medium_1E6_auto.sh
+++ tune_medium_1E6_auto.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# startup runs
+
+python training.py --tune_model --tune_reward --preset QCB_resimulated_medium --model DQN --results tune/QCB_resimulated_medium/metric_f1_timesteps_1000000_alpha_auto_beta_auto --timesteps 1E6 --n_trials 30 --n_eval_episodes 1 --eval_freq 100E6 --eval_metric f1
+
+python training.py --tune_model --tune_reward --preset QCB_resimulated_medium --model PPO --results tune/QCB_resimulated_medium/metric_f1_timesteps_1000000_alpha_auto_beta_auto --timesteps 1E6 --n_trials 30 --n_eval_episodes 1 --eval_freq 100E6 --eval_metric f1
+
+python training.py --tune_model --tune_reward --preset QCB_resimulated_medium --model RecurrentPPO --results tune/QCB_resimulated_medium/metric_f1_timesteps_1000000_alpha_auto_beta_auto --timesteps 1E6 --n_trials 30 --n_eval_episodes 1 --eval_freq 100E6 --eval_metric f1
+
+# actual runs
+
 python training.py --tune_model --tune_reward --preset QCB_resimulated_medium --model DQN --results tune/QCB_resimulated_medium/metric_f1_timesteps_1000000_alpha_auto_beta_auto --timesteps 1E6 --n_trials 30 --n_eval_episodes 10 --eval_freq 2E5 --eval_metric f1
 
 python training.py --tune_model --tune_reward --preset QCB_resimulated_medium --model PPO --results tune/QCB_resimulated_medium/metric_f1_timesteps_1000000_alpha_auto_beta_auto --timesteps 1E6 --n_trials 30 --n_eval_episodes 10 --eval_freq 2E5 --eval_metric f1
