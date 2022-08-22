f5376c5ad136535d64b67c741b28be6d95878a35
Joe Wandy <joewandy@gmail.com>
2022-08-22 03:22:02 +0100

    Script update

--- tune_small_2E5.sh
+++ tune_small_2E5.sh
@@ -2,8 +2,8 @@
 
 # increasing intensity models
 
-python training.py --tune_model --alpha 0.50 --beta 1.00 --preset QCB_chems_small --model DQN --results tune/QCB_chems_small/metric_reward_timesteps_200000_alpha_0.50_beta_1.00 --timesteps 2E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric reward
+python training.py --tune_model --alpha 0.25 --beta 1.00 --preset QCB_chems_small --model DQN --results tune/QCB_chems_small/metric_reward_timesteps_200000_alpha_0.25_beta_1.00 --timesteps 2E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric reward
 
-python training.py --tune_model --alpha 0.50 --beta 1.00 --preset QCB_chems_small --model PPO --results tune/QCB_chems_small/metric_reward_timesteps_200000_alpha_0.50_beta_1.00 --timesteps 2E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric reward
+python training.py --tune_model --alpha 0.25 --beta 1.00 --preset QCB_chems_small --model PPO --results tune/QCB_chems_small/metric_reward_timesteps_200000_alpha_0.25_beta_1.00 --timesteps 2E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric reward
 
-python training.py --tune_model --alpha 0.50 --beta 1.00 --preset QCB_chems_small --model RecurrentPPO --results tune/QCB_chems_small/metric_reward_timesteps_200000_alpha_0.50_beta_1.00 --timesteps 2E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric reward
+python training.py --tune_model --alpha 0.25 --beta 1.00 --preset QCB_chems_small --model RecurrentPPO --results tune/QCB_chems_small/metric_reward_timesteps_200000_alpha_0.25_beta_1.00 --timesteps 2E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric reward
