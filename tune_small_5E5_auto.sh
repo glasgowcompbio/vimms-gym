312132e500f7505641323d75b1e664caf7aabf22
Joe Wandy <joewandy@gmail.com>
2022-08-22 22:37:21 +0100

    Added script

---
+++ tune_small_5E5_auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python training.py --tune_model --tune_reward --preset QCB_chems_small --model DQN --results tune/QCB_chems_small/metric_reward_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric f1
+
+python training.py --tune_model --tune_reward --preset QCB_chems_small --model PPO --results tune/QCB_chems_small/metric_reward_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric f1
+
+python training.py --tune_model --tune_reward --preset QCB_chems_small --model RecurrentPPO --results tune/QCB_chems_small/metric_reward_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 10 --eval_freq 1E6 --eval_metric f1
