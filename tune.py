Fixed evaluation calculation. Removed outdated result files

tune.py
@@ -239,20 +239,6 @@ class TrialEvalCallback(EvalCallback):
                 self.evaluations_results.append(episode_rewards)
                 self.evaluations_length.append(episode_lengths)
 
-                kwargs = {}
-                # Save success log if present
-                if len(self._is_success_buffer) > 0:
-                    self.evaluations_successes.append(self._is_success_buffer)
-                    kwargs = dict(successes=self.evaluations_successes)
-
-                # np.savez(
-                #     self.log_path,
-                #     timesteps=self.evaluations_timesteps,
-                #     results=self.evaluations_results,
-                #     ep_lengths=self.evaluations_length,
-                #     **kwargs,
-                # )
-
             mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
             mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
             self.last_mean_reward = mean_reward
@@ -261,15 +247,10 @@ class TrialEvalCallback(EvalCallback):
                 print(
                     f"Eval num_timesteps={self.num_timesteps}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
                 print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
+
             # Add to current Logger
             self.logger.record("eval/mean_reward", float(mean_reward))
             self.logger.record("eval/mean_ep_length", mean_ep_length)
-
-            if len(self._is_success_buffer) > 0:
-                success_rate = np.mean(self._is_success_buffer)
-                if self.verbose > 0:
-                    print(f"Success rate: {100 * success_rate:.2f}%")
-                self.logger.record("eval/success_rate", success_rate)
 
             # Dump log so the evaluation results are printed with the correct timestep
             self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")

vimms_gym/evaluation.py
@@ -81,18 +81,17 @@ def evaluate(env, intensity_threshold=0.5, format_output=True):
         frag_events = ms2_scan.fragevent
         if frag_events is not None:  # if a chemical has been fragmented ...
 
-            # get the frag event for this scan
-            # TODO: assume only 1 chemical has been fragmented
-            # works for DDA but not for DIA
-            event = frag_events[0]
+            # get the frag events for this scan
+            # there would be one frag event for each chemical fragmented in this MS2 scan
+            for event in frag_events:
 
-            # get the base chemical that was fragmented
-            base_chem = event.chem.get_original_parent()
+                # get the base chemical that was fragmented
+                base_chem = event.chem.get_original_parent()
 
-            # store the max intensity of fragmentation for this base chem
-            parent_intensity = event.parents_intensity[0]
-            fragmented_intensities[base_chem] = max(
-                parent_intensity, fragmented_intensities[base_chem])
+                # store the max intensity of fragmentation for this base chem
+                parent_intensity = event.parents_intensity[0]
+                fragmented_intensities[base_chem] = max(
+                    parent_intensity, fragmented_intensities[base_chem])
 
     TP = 0  # chemicals hit correctly (above threshold)
     FP = 0  # chemicals hit incorrectly (below threshold)
@@ -112,19 +111,16 @@ def evaluate(env, intensity_threshold=0.5, format_output=True):
 
     assert (TP+FP+FN) == len(all_chems)
     assert len(total_frag_intensities) == len(all_chems)
-    recalculated_coverage_prop = (TP+FP)/(TP+FP+FN)
-    recalculated_intensity_prop = np.mean(total_frag_intensities)
 
-    # print('recalculated_coverage_prop', recalculated_coverage_prop, 
-    #     'evaluated_coverage_prop', vimms_env_res['coverage_proportion'][0])
-
-    # print('recalculated_intensity_prop', recalculated_intensity_prop, 
-    #     'evaluated_intensity_prop', vimms_env_res['intensity_proportion'][0])
-
-    # coverage_prop = recalculated_coverage_prop
-    # intensity_prop = recalculated_intensity_prop
+    # ensure that coverage proportion calculation is consistent with ViMMS
     coverage_prop = vimms_env_res['coverage_proportion'][0]
+    recalculated_coverage_prop = (TP+FP)/(TP+FP+FN)
+    assert coverage_prop == recalculated_coverage_prop
+
+    # ensure that intensity proportion calculation is consistent with ViMMS
     intensity_prop = vimms_env_res['intensity_proportion'][0]
+    recalculated_intensity_prop = np.mean(total_frag_intensities)
+    assert intensity_prop == recalculated_intensity_prop
 
     # compute precision, recall, f1
     try: