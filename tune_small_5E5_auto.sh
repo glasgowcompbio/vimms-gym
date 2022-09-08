ca5e52af34b80ddda85364a6f9b81d2398dfdc78
Joe Wandy <joewandy@gmail.com>
2022-09-08 10:55:24 +0100

    Updated scripts

--- tune_small_5E5_auto.sh
+++ tune_small_5E5_auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python training.py --tune_model --tune_reward --preset QCB_chems_small --model DQN --results tune/QCB_chems_small/metric_f1_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 30 --eval_freq 1E5 --eval_metric f1
+python training.py --tune_model --tune_reward --preset QCB_chems_small --model DQN --results tune/QCB_chems_small/metric_f1_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 20 --eval_freq 1E5 --eval_metric f1
 
-# python training.py --tune_model --tune_reward --preset QCB_chems_small --model PPO --results tune/QCB_chems_small/metric_f1_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 30 --eval_freq 1E5 --eval_metric f1
+# python training.py --tune_model --tune_reward --preset QCB_chems_small --model PPO --results tune/QCB_chems_small/metric_f1_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 20 --eval_freq 1E5 --eval_metric f1
 
-# python training.py --tune_model --tune_reward --preset QCB_chems_small --model RecurrentPPO --results tune/QCB_chems_small/metric_f1_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 30 --n_eval_episodes 30 --eval_freq 1E5 --eval_metric f1
+# python training.py --tune_model --tune_reward --preset QCB_chems_small --model RecurrentPPO --results tune/QCB_chems_small/metric_f1_timesteps_500000_alpha_auto_beta_auto --timesteps 5E5 --n_trials 20 --n_eval_episodes 30 --eval_freq 1E5 --eval_metric f1
