c8daae8a1d6ccf81457f3bf54ab5e4407bbe6d57
Joe Wandy <joewandy@gmail.com>
2022-11-07 15:08:02 +0000

    Updated scripts and notebook

--- train_medium.sh
+++ train_medium.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-python training.py --preset QCB_resimulated_medium --model DQN --timesteps 1E6 --results notebooks/QCB_resimulated_medium --alpha 0.25 --beta 0.00
-python training.py --preset QCB_resimulated_medium --model DQN --timesteps 1E6 --results notebooks/QCB_resimulated_medium --alpha 0.5 --beta 0.00
-python training.py --preset QCB_resimulated_medium --model DQN --timesteps 1E6 --results notebooks/QCB_resimulated_medium --alpha 0.75 --beta 0.00
-python training.py --preset QCB_resimulated_medium --model DQN --timesteps 1E6 --results notebooks/QCB_resimulated_medium --alpha 0.25 --beta 0.50
+env_name="DDAEnv"
+model_name="DQN"
+timesteps=20E6
+repeat=1
+verbose=2
+preset="QCB_resimulated_medium"
+alphas=( 0.00 0.25 0.50 0.75 1.00 )
+betas=( 0.50 )
+horizons=( 1 4 )
+
+for (( i=0; i<${repeat}; i++ ))
+do
+  results="notebooks/${preset}/${i}"
+  for alpha in "${alphas[@]}"
+  do
+    for beta in "${betas[@]}"
+    do
+      for horizon in "${horizons[@]}"
+      do
+        out_file="${env_name}_${model_name}_alpha_${alpha}_beta_${beta}_horizon_${horizon}.zip"
+        echo "Training ${out_file}"
+#        python training.py --preset {preset} --model ${model_name} --timesteps ${timesteps} --results ${results} --verbose ${verbose} --out_file ${out_file} --horizon ${horizon}
+      done
+    done
+  done
+done
