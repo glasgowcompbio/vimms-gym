8560c81e4cb97db086de2a37153f9e7531b73c0a
Joe Wandy <joewandy@gmail.com>
2023-02-20 23:47:00 +0000

    Updated script

--- tune_medium.sh
+++ tune_medium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python training.py --tune_model --preset QCB_resimulated_medium --model DQN --results tune/QCB_resimulated_medium/metric_reward_timesteps_1.0E6_alpha_0.50_beta_0.50 --alpha 0.50 --beta 0.50 --timesteps 1E6 --n_trials 20 --n_eval_episodes 30 --eval_freq 2E5 --eval_metric reward
+python training.py --tune_model --preset QCB_resimulated_medium --model DQN --results tune/QCB_resimulated_medium/metric_reward_timesteps_1.0E6_alpha_0.50_beta_0.50 --alpha 0.50 --beta 0.50 --timesteps 1E6 --n_trials 20 --n_eval_episodes 30 --eval_freq 2E5 --eval_metric reward --horizon 4
