56a946d5caaa7e272b270ae5bba365af30f63219
Joe Wandy <joewandy@gmail.com>
2023-03-28 12:23:50 +0100

    Removed low n_steps (too slow)

--- tune.py
+++ tune.py
@@ -77,7 +77,7 @@ def sample_ppo_params(trial, tune_model, tune_reward):
 
     if tune_model:
         batch_size = trial.suggest_categorical('batch_size', [8, 16, 32, 64, 128, 256, 512])
-        n_steps = trial.suggest_categorical('n_steps', [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
+        n_steps = trial.suggest_categorical('n_steps', [128, 256, 512, 1024, 2048])
         gamma = trial.suggest_categorical('gamma', [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
         learning_rate = trial.suggest_float('learning_rate', 1e-5, 1, log=True)
         lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
